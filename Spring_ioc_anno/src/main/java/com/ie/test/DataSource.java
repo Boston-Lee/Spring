32e6491881041a31592344eb3a0c927141976133
Boston <750604503@qq.com>
2021-02-08 10:42:21 +0800

    Commit Message

---
+++ Spring_ioc_anno/src/main/java/com/ie/test/DataSource.java
@@ -0,0 +1,42 @@
+package com.ie.test;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.junit.jupiter.api.Test;
+
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.util.ResourceBundle;
+
+public class DataSource {
+    /* public static void main(String[] args) {
+
+     }*/
+    @Test
+//    手动配置数据源(加载配置文件)
+    public void test1() throws Exception {
+        /*ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test");
+        dataSource.setUser("root");
+        dataSource.setPassword("123");
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();*/
+//        读取配置文件
+        ResourceBundle rb = ResourceBundle.getBundle("jdbc");
+        String driver = rb.getString("jdbc.driver");
+        String url = rb.getString("jdbc.url");
+        String username = rb.getString("jdbc.username");
+        String password = rb.getString("jdbc.password");
+//        创建数据源对象
+        ComboPooledDataSource dataSource=new ComboPooledDataSource();
+        dataSource.setDriverClass(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+
+    }
+}
